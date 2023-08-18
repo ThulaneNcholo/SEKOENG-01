97e8bf9f0e957a160dbc87b3e43a5392995c31f0
thulane <thulanencholo95@gmail.com>
2023-08-18 12:02:50 +0200

    commit

--- server/server/settings.py
+++ server/server/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ebf#$297*elaln**cmx62ilinzc_s3#r7#1@@9r_0895%fe9b='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
